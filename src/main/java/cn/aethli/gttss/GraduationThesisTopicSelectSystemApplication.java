b8b13b35c752b140036c780cf9ef18b4b42ede1f
YRJ <931622815@qq.com>
2019-03-04 14:34:27 +0800

    Initial commit

---
+++ src/main/java/cn/aethli/gttss/GraduationThesisTopicSelectSystemApplication.java
@@ -0,0 +1,13 @@
+package cn.aethli.gttss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationThesisTopicSelectSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationThesisTopicSelectSystemApplication.class, args);
+    }
+
+}
